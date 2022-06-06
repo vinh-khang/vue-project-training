041734610dcb9aeeed25e817f8ce5523a961bf5a
nvkhang <captainvietnam2016@gmail.com>
2022-06-06 09:20:18 +0700

    Breakpoints and Visibility code update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,8 @@
 <template>
 <div class="home">
-  <h1>Hello from Homepage</h1>
- <template>
-  <div class="home">
-    <h1>Homepage</h1>
-    <v-btn class="pink white--text">click me</v-btn>
-    <v-btn depressed color="pink">click me</v-btn>
-
-    <v-btn flat class="pink green--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>email me</span>
-    </v-btn>
-
-    <v-btn depressed large class="blue white--text">
-      <span>email me</span>
-      <v-icon right large>mdi-email</v-icon>
-    </v-btn>
-
-  </div>
-</template> 
+ <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
+    <v-btn class="hidden-md-and-up">click me</v-btn>
+    <!-- <v-btn class="hidden-sm-only">click me</v-btn> -->
 </div>
   
 </template>
