38775e5e6356cc05efc367042f4b8c03bf80188d
nvkhang <captainvietnam2016@gmail.com>
2022-06-07 01:33:54 +0700

    navigation drawers code update

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -1,15 +1,33 @@
 <template>
     <nav>
-        <v-toolbar app>
-            <v-toolbar-title class="text-uppercase grey--text">
+        <v-app-bar app>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-app-bar-title class="text-uppercase grey--text">
                 <span class="font-weight-light">Todo</span>
                 <span>Ninja</span>
-            </v-toolbar-title>
+            </v-app-bar-title>
             <v-spacer></v-spacer>
             <v-btn outlined color="grey">
                 <span>Sign Out</span>
                 <v-icon right>mdi-exit-to-app</v-icon>
             </v-btn>
-        </v-toolbar>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" app class="pink">
+        <p1>
+            Khang
+        </p1>
+        </v-navigation-drawer>
     </nav>
 </template>
+
+<script>
+
+export default {
+
+    data(){
+        return {
+            drawer: false,
+        }
+    }
+}
+</script>
