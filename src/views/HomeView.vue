589ef869f2922274b81438dc1c34a548beec8a24
nvkhang <captainvietnam2016@gmail.com>
2022-06-06 09:10:04 +0700

    button and icon code update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,24 @@
 <template>
 <div class="home">
   <h1>Hello from Homepage</h1>
-  <h1 class="red white--text">Hello from Homepage</h1>
-  <h1 class="red lighten-4 white--text">Hello from Homepage</h1>
-  <h1 class="red lighten-1 pink--text text--darken-4">Hello from Homepage</h1>
-  <h1 class="display-2">Hello from Homepage</h1>
-  <h1 class="headline">Hello from Homepage</h1>
-  <h1 class="subtitle-1">Hello from Homepage</h1>
-  <h1 class="overline">Hello from Homepage</h1>
+ <template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed color="pink">click me</v-btn>
+
+    <v-btn flat class="pink green--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="blue white--text">
+      <span>email me</span>
+      <v-icon right large>mdi-email</v-icon>
+    </v-btn>
+
+  </div>
+</template> 
 </div>
   
 </template>
