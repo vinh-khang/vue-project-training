aa5ee7bbc9ab06a2ef7487405604b5deabc820f7
nvkhang <captainvietnam2016@gmail.com>
2022-06-06 08:30:18 +0700

    Text and color code update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,14 @@
 <template>
+<div class="home">
   <h1>Hello from Homepage</h1>
+  <h1 class="red white--text">Hello from Homepage</h1>
+  <h1 class="red lighten-4 white--text">Hello from Homepage</h1>
+  <h1 class="red lighten-1 pink--text text--darken-4">Hello from Homepage</h1>
+  <h1 class="display-2">Hello from Homepage</h1>
+  <h1 class="headline">Hello from Homepage</h1>
+  <h1 class="subtitle-1">Hello from Homepage</h1>
+  <h1 class="overline">Hello from Homepage</h1>
+</div>
+  
 </template>
 
