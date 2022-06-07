3df27dbdeb845742bd5497682534d8a7c27bae74
nvkhang <captainvietnam2016@gmail.com>
2022-06-07 19:10:09 +0700

    menu code update

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -7,6 +7,21 @@
                 <span>Ninja</span>
             </v-app-bar-title>
             <v-spacer></v-spacer>
+
+            <v-menu offset-y>
+                <template v-slot:activator="{ on }">
+                    <v-btn text v-on="on" color="grey">
+                        <v-icon left color="grey">mdi-chevron-down</v-icon>
+                        <span>Menu</span>
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                        <v-list-item-title>{{ link.text }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+            
             <v-btn outlined color="grey">
                 <span>Sign Out</span>
                 <v-icon right>mdi-exit-to-app</v-icon>
