bc6da6bd4f319a46732c8616ba1671a3ccd9815e
nvkhang <captainvietnam2016@gmail.com>
2022-06-07 20:43:06 +0700

    datepicker code update

--- src/components/PopupView.vue
+++ src/components/PopupView.vue
@@ -22,6 +22,12 @@
             <v-form class="px-3">
                 <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
                 <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil"></v-textarea>
+                <v-menu max-width="290">
+                    <template v-slot:activator="{ on }">
+                        <v-text-field :value="formattedDate" label="Due date" prepend-icon="mdi-calendar-range" v-on="on"></v-text-field>
+                    </template>
+                    <v-date-picker v-model="due"></v-date-picker>
+                </v-menu>
                 <v-btn text class="info mx-0 mt-3" @click="submit">Add project</v-btn>
             </v-form>
         </v-card-text>
@@ -31,17 +37,24 @@
 </template>
 
 <script>
+import moment from 'moment';
 export default {
     data(){
         return {
             title: '',
-            content: ''
+            content: '',
+            due: ''
         }
     },
     methods: {
         submit(){
             console.log(this.title, this.content);
         }
+    },
+     computed: {
+    formattedDate() {
+      return this.due ? moment(this.due).format("Do MMMM YYYY") : "";
     }
+  }
 }
 </script>
